8fd72a440a11f01650f36d0160c83aed9372ffc0
BrunoFrad <brunfradrib@gmail.com>
2025-06-19 11:03:38 -0300

    Fix the navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,17 +2,22 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
 
     const router = useRouter();
+    const pathname = usePathname();
 
     return(
         <nav className="flex h-20 items-center justify-between border-b-2">
             <section className="flex gap-10">
                 <img src={"vercel.svg"} width={"30vw"} className="ml-16" />
                 <ul className="flex space-x-4 gap-2">
-                    <li><a href="/" className="text-md text-neutral-100 font-medium hover:cursor-default">Player</a></li>
+                    {pathname == "/" ? 
+                        <li><a href="#" className="text-md text-neutral-100 font-medium hover:cursor-default">Player</a></li> :
+                        <li><a href="/" className="text-md text-neutral-500 hover:text-neutral-100">Player</a></li>
+                    }
                     <li><a href="https://github.com/BrunoFrad/yamp" target="_blank" className="text-md text-neutral-500 hover:text-neutral-100">Github</a></li>
                 </ul>
             </section>
