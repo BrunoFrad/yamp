7ba048af16d025f7612723374405b897e478a394
BrunoFrad <brunfradrib@gmail.com>
2025-06-21 17:33:59 -0300

    Craate the "Add Music" Form

---
+++ src/app/components/thumb-uploader.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
+
+export default function MusicUploader() {
+
+    const [thumb, setThumb] = useState<File | undefined>();
+    const [preview, setPreview] = useState<string | null>(null);
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    function handleAddThumbnail() {
+        fileInputRef.current?.click();
+    };
+    
+    function handleFileChange() {
+        const selectedFile = fileInputRef.current?.files?.[0];
+        if (selectedFile) {
+            setThumb(selectedFile);
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                setPreview(reader.result as string);
+            };
+
+            reader.readAsDataURL(selectedFile);
+        }
+    }
+
+    return(
+        <>
+            <input
+                type="file"
+                ref={fileInputRef}
+                accept="image/*"
+                onChange={handleFileChange}
+                className="hidden"
+            />
+            <div className="group flex justify-center items-center w-[33vh] h-[33vh] border-2 rounded-xl overflow-hidden relative">
+                {preview ? (
+                    <>
+                        <img
+                            src={preview}
+                            onError={(e) => (e.currentTarget.style.display = 'none')}
+                            className="w-full h-full object-cover"
+                            alt="Preview"
+                        />
+                        <Button
+                            onClick={handleAddThumbnail}
+                            className="absolute hidden group-hover:flex"
+                        >
+                            Change Image
+                        </Button>
+                    </>
+                ) : (
+                    <Button onClick={handleAddThumbnail}>Add Image</Button>
+                )}
+            </div>
+        </>
+    );
+
+}
--- src/app/contribute/page.tsx
+++ src/app/contribute/page.tsx
@@ -3,11 +3,15 @@
 import { Button } from "@/components/ui/button";
 import ContributeContainer from "../components/contribute-container";
 import { useRef, useState } from "react";
-import axios from "axios";
+import MusicUploader from "../components/thumb-uploader";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
 
 export default function Contribute () {
 
     const fileInputRef = useRef<HTMLInputElement | null>(null);
+    const [musicTitle, setMusicTitle] = useState<string | undefined>();
+    const [musicDescription, setMusicDescription] = useState<string>("");
     const [file, setFile] = useState<File | undefined>();
     
     function handleAddMusicClick () {
@@ -18,15 +22,8 @@ export default function Contribute () {
         const selectedFile = fileInputRef.current?.files?.[0];
         if (selectedFile) {
             setFile(selectedFile);
-
-            const formData = new FormData();
-            formData.append('file', selectedFile);
-
-            axios.post('/api/upload', formData, {
-                headers: { 'Content-Type': 'multipart/form-data' }
-            });
+            setMusicTitle(selectedFile?.name.split('.')[0]);
         }
-
     }
 
     return(
@@ -38,15 +35,35 @@ export default function Contribute () {
                 onChange={handleFileChange}
                 className="hidden"
             />
-            {!file ? 
-                <div className="flex flex-col h-full justify-center items-center">
-                    <ContributeContainer>
-                        <h1 className="scroll-m-20 text-center text-5xl font-extrabold tracking-tight text-balance">Thanks by helping our web app</h1>
-                        <Button onClick={handleAddMusicClick}>Add a new music file</Button>
-                    </ContributeContainer>
-                </div> :
-                <></>
-            }
+            <div className="flex flex-col h-full justify-center items-center">
+                {
+                    !file ?
+                        <ContributeContainer>
+                            <h1 className="scroll-m-20 text-center text-5xl font-extrabold tracking-tight text-balance">Thanks by helping our web app</h1>
+                            <Button onClick={handleAddMusicClick}>Add a new music file</Button>
+                        </ContributeContainer>
+                        :
+                        <ContributeContainer >
+                            <div className="flex gap-10">
+                            <MusicUploader />
+                            <div>
+                                <div className="flex flex-col gap-4 py-4">
+                                    <Label className="font-medium">Music Title</Label>
+                                    <Input value={musicTitle} onChange={(e) => setMusicTitle(e.currentTarget.value)} className="w-[30vw]" />
+                                </div>
+                                <div className="flex flex-col gap-4 py-4">
+                                    <Label className="font-medium">Music Description</Label>
+                                    <Input className="w-[30vw]" value={musicDescription} onChange={(e) => setMusicDescription(e.currentTarget.value)} />
+                                </div>
+                                <div className="flex items-center h-[10vh] gap-4">
+                                    <Button onClick={handleAddMusicClick}>Change File</Button>
+                                    <Button>Finish</Button>
+                                </div>
+                            </div>
+                            </div>
+                        </ContributeContainer>
+                }
+            </div>
         </>
     );
 }
